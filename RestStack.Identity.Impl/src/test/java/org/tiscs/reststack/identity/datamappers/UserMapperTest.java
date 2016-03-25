dd540bd6932606a6440511c82e2cf4e23545c9be
Tiscs <tiscs@live.com>
2016-03-25 12:00:41 +0800

    Add bean injection sample to "UserMapperTest.java";

--- RestStack.Identity.Impl/src/test/java/org/tiscs/reststack/identity/datamappers/UserMapperTest.java
+++ RestStack.Identity.Impl/src/test/java/org/tiscs/reststack/identity/datamappers/UserMapperTest.java
@@ -1,24 +1,33 @@
 package org.tiscs.reststack.identity.datamappers;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.tiscs.reststack.core.contexts.AppContext;
 import org.tiscs.reststack.core.contexts.DbContext;
+import org.tiscs.reststack.core.contexts.WebContext;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AppContext.class, DbContext.class}, loader = AnnotationConfigContextLoader.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppContext.class, DbContext.class, WebContext.class})
 public class UserMapperTest {
     @Autowired
-    private BasicDataSource dataSource;
+    private ApplicationContext applicationContext;
+    private UserMapper userMapper;
+
+    @Before
+    public void setUp() throws Exception {
+        this.userMapper = applicationContext.getBean(UserMapper.class);
+    }
 
     @org.junit.Test
     public void findAll() throws Exception {
-        assertNotNull(dataSource);
+        assertNotNull(userMapper);
     }
 }
